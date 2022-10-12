f3e3f548455a7c8b179debe67f58c37ec4f622fb
Nastia Sydorchuk <nastia.sydorchuk@gmail.com>
2022-10-12 16:51:31 +0100

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import './App.scss';
+import { HomePage } from './Pages/HomePage';
+import { PhonesPage } from './Pages/PhonesPage';
+import { TabletsPage } from './Pages/TabletsPage';
+import { AccessoriesPage } from './Pages/AccessoriesPage';
+import { ProductDetailsPage } from './Pages/ProductDetailsPage';
+import { FavouritesPage } from './Pages/FavouritesPage';
+import { CartPage } from './Pages/CartPage';
+import { NotFoundPage } from './Pages/NotFoundPage';
+import { Footer } from './Components/Footer';
+import { CartContextProvider } from './Components/Context/CartContextProvider';
+import { FavContextProvider } from './Components/Context/FavContextProvider';
+import { QueryContextProvider } from './Components/Context/QueryContext';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryContextProvider>
+      <CartContextProvider>
+        <FavContextProvider>
+          <div className="App">
+            <div className="App__main">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/phones">
+                  <Route index element={<PhonesPage />} />
+                  <Route path=":productId" element={<ProductDetailsPage />} />
+                </Route>
+                <Route path="/tablets">
+                  <Route index element={<TabletsPage />} />
+                  <Route path=":productId" element={<ProductDetailsPage />} />
+                </Route>
+                <Route path="/accessories" element={<AccessoriesPage />} />
+                <Route path="/cart" element={<CartPage />} />
+                <Route path="/favourites" element={<FavouritesPage />} />
+                <Route path="*" element={<NotFoundPage />} />
+              </Routes>
+            </div>
+            <Footer />
+          </div>
+        </FavContextProvider>
+      </CartContextProvider>
+    </QueryContextProvider>
   );
-}
+};
 
 export default App;
